bed2b5299ac1e7bec07bf77ffa5589612cd1536f
doannt <doannt@unicloud.com.vn>
2022-07-06 16:28:29 +0700

    update

--- index.js
+++ index.js
@@ -76,3 +76,20 @@ sunshine = [
     },
   ]
 
+function sleep(ms){
+       return new Promise(function(resolve){
+            setTimeout(resolve, ms)
+       }); 
+}
+    sleep(1000)
+    .then(function(){
+        console.log(1)
+        return sleep(1000)
+    })
+    .then(function(){
+        console.log(2)
+        return sleep(1000)
+    })
+    .then(function(){
+        console.log(3)
+    })
