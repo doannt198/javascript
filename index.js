dec2d0161e398dfc1434c65076efce820a889bc2
doannt <doannt@unicloud.com.vn>
2022-07-18 16:23:57 +0700

    update

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
+/* Quan sát expected results và hoàn thành code còn thiếu để vượt qua thử thách này. */
+// Expected results:
+/* var arr =[1,2,3]
 
-
-function handlePrint() {
-  return window.print()
+function total(m) {
+  return m.reduce((a,b)=> a+b) 
 }
+console.log(total(arr)) */
+
+// Expected results:
+var arr = [
+  ['name', 'abc'],
+  ['age', 18],
+];
+
+function arrToObj(ketqua) {
+    var result = ketqua.reduce((total, [key, value])=> Object.assign(total, {[key]:value}), {});
+    return result
+}
+console.log(arrToObj(arr))
