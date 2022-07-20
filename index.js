f65c87c9c874600fee02299ec514c0e401a8eb94
doannt <doannt@unicloud.com.vn>
2022-07-20 17:50:59 +0700

    update

--- index.js
+++ index.js
@@ -1,20 +1,32 @@
-/* Quan sát expected results và hoàn thành code còn thiếu để vượt qua thử thách này. */
-// Expected results:
-/* var arr =[1,2,3]
+/* concat(): Nối mảng a vs mảng b ,
+  copyWithin(): Copy phần tủ của mảng và ghi đè lên vị trí được cài đặt (vị trí ghi đè, vị trí bắt đầu copy, vị trí kết thúc copy),
+   entries():  , 
+   every(): kiểm tra các phần tủ trong 1 mảng thoả mãn 1 logic cho trước, trả về true nếu tất cả hợp lệ và false nếu không hợp lệ  
+   fill() thay đổi giá trị của tất cả  phần tử trong mảng về cùng 1 giá trị., 
+  filter() x
+, find(), x
+findIndex(),x
+ forEAch() ,x
+ from() ,x
+ includes() x, 
+ indexOf() x, isArray(),x
+join()x ,keys()x lastindexof(), lengthx, map()x,  pop()x, prototype() , push(), reduce() 
+reduceRight(), reverse(), shift()x, slice()x, some()x , sort()x, splice(), toString(), unshift(), Valueof() */
 
-function total(m) {
-  return m.reduce((a,b)=> a+b) 
+var a = ['1', '2', '4', '6'] 
+var b = ['a', 'c', 'f', 'e', 'f', 'h']
+var c = {
+  name: " abc",
+  age:20 
 }
-console.log(total(arr)) */
+var data = [
+  {name :"a", age: 12},
+  {name :"b", age: 13},
+  {name :"c", age: 14},
+  {name :"d", age: 15},
+]
 
-// Expected results:
-var arr = [
-  ['name', 'abc'],
-  ['age', 18],
-];
-
-function arrToObj(ketqua) {
-    var result = ketqua.reduce((total, [key, value])=> Object.assign(total, {[key]:value}), {});
-    return result
-}
-console.log(arrToObj(arr))
+var results = data.some((t)=> {
+  
+})
+console.log(results)
